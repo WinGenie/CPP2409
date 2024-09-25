9c638ccc979193d937e30949168613375442e8c6
WinGenie <seungjin7151@gmail.com>
2024-09-25 16:42:52 +0900

    Loop Exercises

--- challenge/week4/count_alpha.cpp
+++ challenge/week4/count_alpha.cpp
@@ -4,36 +4,25 @@ using namespace std;
 int main(){
     int vowel = 0;
     int consonant = 0;
-    
-    cout << "영문자를 입력하고 ctrl+z를 치세요" << endl;
+    char ch;
+    while (true) {
+        cout << "영문자를 입력하세요: ";
+        cin >> ch;
 
-    char ch; //사용자 입력 저장하기 위한 변수
-
-    while(cin >> ch){
-        switch (ch)
-        {
-        case 'a': 
-            vowel++;
-            break;
-        case 'e': 
-            vowel++;
-            break;
-        case 'i': 
-            vowel++;
-            break;
-        case 'o': 
-            vowel++;
-            break;
-        case 'u': 
-            vowel++; 
-            break;   
-        default:
-            consonant++;
-            break;
+        if (!(ch >= 'a' && ch <= 'z')) {
+            cout << "유효하지 않은 입력입니다. 소문자 알파벳을 입력하세요.\n";
+            continue;
         }
+
+        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+            vowel++;
+        } else {
+            consonant++;
+        }
+
+        cout << "모음의 수: " << vowel << "\n";
+        cout << "자음의 수: " << consonant << "\n";
     }
-    cout << "모음 : " << vowel << endl;
-    cout << "자음 : " << consonant << endl;
 
     return 0;
 }
