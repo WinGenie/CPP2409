271dcd9ce4548ee175d7fdd8e5e46098a76f056a
WinGenie <seungjin7151@gmail.com>
2024-10-02 11:09:03 +0900

    Array Exercises

--- challenge/week5/select_customer2.cpp
+++ challenge/week5/select_customer2.cpp
@@ -6,9 +6,10 @@ int main(){
     const int maxPeople = 5;
     string names[maxPeople];
     int ages[maxPeople];
-    int userinput;
-    int max;
-    int min;
+
+    int maxAgeIndices[maxPeople];
+    int minAgeIndices[maxPeople];
+    int maxCount = 0, minCount = 0;
 
     cout << maxPeople << "명의 회원 정보를 입력해주세요." << endl;
 
@@ -18,39 +19,62 @@ int main(){
         cout << "사람 " << i+1 << "의 나이: ";
         cin >> ages[i];
     }
-    cout << endl;
     
-
-
-    cout << "선택해주세요: (1)가장 나이가 많은 사람 출력 (2) 가장 나이가 적은 사람 출력 (3) 종료" << endl;
-    cin >> userinput;
-      
-    //while(userinput==true)
-    switch (userinput)
-    {
-    case 1:
-        max = ages[0];
-        for (auto elem : ages){
-        if (elem > max)
-            max = elem;
-    }
-    cout << "최대값=" << max << "이름은" << names[max] << endl;
-        break;
-    case 2:
-        min = ages[0];
-        for (auto elem : ages){
-            if (elem < min)
-            {
-            min = elem;           
-            }
-    cout << "최소값=" << min << "이름은" << names[min] << endl;           
+    int maxAge = ages[0];
+    for ( int i = 1; i < maxPeople; i++){
+        if (ages[i] > maxAge){
+            maxAge = ages[i];
         }
-    case 3:
-        cout << "종료합니다";
-        break;
-    default:
-        cout << "다른 메뉴를 선택해주세요";
-    }
     }
 
-    
+    int minAge = ages[0];
+    for (int i = 1; i < maxPeople; i++){
+        if (ages[i] < minAge){
+            minAge = ages[i];
+        }
+    }
+
+    for (int i = 0; i < maxPeople; i++){
+        if (ages[i] == maxAge){
+            maxAgeIndices[maxCount++] = i;
+        }
+        if (ages[i] == minAge){
+            minAgeIndices[minCount++] = i;
+        }
+    }
+
+    while(1){
+        cout << "\n메뉴를 선택하세요:\n";
+        cout << "1. 가장 나이가 많은 사람 출력\n";
+        cout << "2. 가장 나이가 적은 사람 출력\n";
+        cout << "3. 종료\n";
+        int choice;
+        cin >> choice;
+
+        if (choice == 1){
+            cout << "가장 나이가 많은 사람: ";
+            for(int i = 0; i< maxCount; i++){
+                cout << names[maxAgeIndices[i]] << "(" << ages[maxAgeIndices[i]] << "세)";
+            }
+            cout << "\n";
+        }
+        else if (choice == 2){
+            cout << "가장 나이가 적은 사람: ";
+            for (int i = 0; i < minCount; i++){
+                cout << names[minAgeIndices[i]] << "(" << ages[minAgeIndices[i]] << "세)";
+            }
+            cout << "\n";
+        }
+        else if (choice == 3){
+            cout << "프로그램을 종료합니다.\n";
+            break;
+        }
+        else{
+            cout << "잘못된 입력입니다. 프로그램을 종료합니다.\n";
+            break;
+        }
+    }
+    return 0;
+
+
+}
