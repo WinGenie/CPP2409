25057134bcc395550cce668769fc2eb3a78f3913
WinGenie <seungjin7151@gmail.com>
2024-10-07 16:11:11 +0900

    Function and header Exercises

---
+++ challenge/week6/recommendation.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+const int NUM_USERS = 3;
+const int NUM_ITEMS = 3;
+int userPreferences[NUM_USERS][NUM_ITEMS];
+
+// 사용자 선호도 입력 함수
+void initializePreferences(int preferences[NUM_USERS][NUM_ITEMS]) {
+    for (int i = 0; i < NUM_USERS; ++i) {
+        cout << "사용자 " << (i + 1) << "의 선호도를 입력하세요 (";
+        cout << NUM_ITEMS << "개의 항목에 대해): ";
+        for (int j = 0; j < NUM_ITEMS; ++j) {
+            cin >> userPreferences[i][j];
+        }
+    }
+}
+
+// 각 사용자에 대한 추천 항목 찾기 함수
+void findRecommendedItems(int preferences[NUM_USERS][NUM_ITEMS]){
+for (int i = 0; i < NUM_USERS; ++i){
+    int maxPreferenceIndex = 0;
+    for (int j = 1; j < NUM_ITEMS; ++j){
+        if (userPreferences[i][j] > userPreferences[i][maxPreferenceIndex]){
+            maxPreferenceIndex = j;
+        }
+    }
+
+    cout << "사용자 " << (i + 1) << "에게 추천하는 항목: ";
+    cout << (maxPreferenceIndex + 1) << std::endl;
+}
+}
+
+int main() {
+    initializePreferences(userPreferences);  // 사용자 선호도 입력
+    findRecommendedItems(userPreferences);  // 추천 항목 출력
+    return 0;
+}
---
+++ practice/week6/myheader.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "myheader.h"
+
+int summation(int value1, int vlaue2){
+    int sum = value1 + vlaue2;
+    return sum;
+}
